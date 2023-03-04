0e738701305fe69445affd9dbe31e83d2e9f62a6
Lucas Machado <lucasmachado.rj@gmail.com>
2023-03-04 19:17:55 -0300

    double quote preference

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,31 +3,33 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: [
-    'plugin:jest/recommended',
-    'standard',
-  ],
-  parser: '@typescript-eslint/parser',
+  extends: ["plugin:jest/recommended", "standard"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: 'module',
+    sourceType: "module",
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
+  plugins: ["@typescript-eslint"],
   rules: {
-    'comma-dangle': ['error', {
-      arrays: 'always-multiline',
-      objects: 'always-multiline',
-      imports: 'always-multiline',
-      exports: 'always-multiline',
-      functions: 'always-multiline',
-    }],
-    camelcase: 'off',
-    'no-warning-comments': 'warn',
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off',
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': ['error', { ignoreRestSiblings: true }],
+    "comma-dangle": [
+      "error",
+      {
+        arrays: "always-multiline",
+        objects: "always-multiline",
+        imports: "always-multiline",
+        exports: "always-multiline",
+        functions: "always-multiline",
+        quotes: "double",
+      },
+    ],
+    camelcase: "off",
+    "no-warning-comments": "warn",
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { ignoreRestSiblings: true },
+    ],
   },
-}
+};
